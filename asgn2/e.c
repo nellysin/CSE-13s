ce2dbdfdf8c42d3b7ed4eee0e2b9ecb18b84a341
Nelly Sin <nesin@ucsc.edu>
2021-10-04 22:06:21 +0000

    update approximate e code

--- asgn2/e.c
+++ asgn2/e.c
@@ -2,32 +2,28 @@
 #include "mathlib.h"
 #include <stdlib.h>
 
-#define EPSILON 1e-14
+/*double e_terms(){
+	static double e = 0.0; // total
+	static int k = 1; // k
+	static double term = 1.0; //current term (computed)
+	static double p_term = 1.0; // previous term
+}*/
 
 double e(){
-	static double e = 0.0;
-	int k = 0;
-	double term = 1.0;
-	double p_term = 0.0;
-        while(term > EPSILON){
-		if(k == 0 || k == 1){
-			p_term = 1;
-			e += p_term;
-		}
-		else{
-			term = 1/k;
-			e += p_term * term;
-			p_term = term;
-		}
+	static double e = 0.0;  // total
+        static int k = 1; // k
+        static double term = 1.0; //current term (computed)
+        static double p_term = 1.0; // previous term
+        while(term > EPSILON){		// it will come to a halt when it reaches close the EPSILON 
+		term = 1.0/k;		// term is the 1/k 
+		e = e + p_term * term; // multiply previous term & current term add it to "e"
+		p_term = term;
 		k += 1;
 	}
         return e;
 }
 
-//double e_terms(){
-//	static double e = 0.0;
-//}
-
 int main(void){
 	printf("%16.151f\n", e());
+	return 0;
 }
