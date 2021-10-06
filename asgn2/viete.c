94f1b6f83f2feeb7a2780907e18132c445f8786d
Nelly Sin <nesin@ucsc.edu>
2021-10-06 23:18:49 +0000

    update viete.c code

--- asgn2/viete.c
+++ asgn2/viete.c
@@ -5,16 +5,15 @@
 static int viete_factors = 0;
 double pi_viete(){
         viete_factors = 0;
-        double k = 2.0;
-//	double n = 1.0;
-        double viete = 1.0;
-        while(k >= EPSILON){
-		k = sqrt_newton(k);
-		viete *= k;
-		viete_factors += 1;
-		printf("%f", k);
+        double k = sqrt_newton(2.0);
+        double viete = sqrt_newton(2.0)/2.0;
+        while(absolute(k/2.0 - 1) >= EPSILON){ // while n approches EPSILON
+		k = sqrt_newton(2.0 + k);
+		viete *= k/2.0;
+
+		viete_factors += 1; 
 	}
-	viete = 2 / viete;
+	viete = 2.0 / viete;
         return viete;
 }
 
@@ -22,8 +21,8 @@ int pi_viete_factors(){
         return viete_factors;
 }
 
-int main(void){
+/*int main(void){
         printf("%16.15lf\n", pi_viete());
         printf("%d\n", pi_viete_factors());
         return 0;
-}
+}*/
