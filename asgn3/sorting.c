2f72c01335f2db9a515f380eff2ef0de05ad3934
Nelly Sin <nesin@ucsc.edu>
2021-10-14 06:51:59 +0000

    using new method & fixing segment fault

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -12,20 +12,34 @@
 #include <stdlib.h>
 
 #define OPTIONS "[-haeinsqn:p:r:] [-n lenth] [-p elements] [-r seed]"
-#define SEED 13371453
-#define LENGTH 100
-#define PRINT 100
-
-
-typedef enum {INSERTION, HEAP, SHELL, QUICK} Sorts;
-const char *names[] = {"Insertion Sort", "Heap Sort", "Shell Sort", "Quick Sort"};
 
 //CITE: Professor Long (referencing previous assignment setup)
 //CITE: Eugene Chou for idea help in section
+static uint32_t SEED = 13371453;
+static uint32_t LENGTH = 100;
+static uint32_t PRINT = 100;
 
 int main(int argc, char **argv) {
+
+	Stats stats;
+        stats.moves = 0;
+        stats.compares = 0;
+
+	uint32_t mask = 0x03fffffff;
+
+	srandom(SEED);
+        uint32_t *A = (uint32_t *)calloc(LENGTH, sizeof(uint32_t));
+        for(uint32_t i = 0; i <= LENGTH; i += 1){
+                A[i]  = random() & mask;
+                // implement a bitmask to fit in 30 bits
+        }
+
+	typedef enum {INSERTION, HEAP, SHELL, QUICK} Sorts;
+        void (*sort[])() = {&insertion_sort};
+	const char *names[] = {"Insertion Sort", "Heap Sort", "Shell Sort", "Quick Sort"};
+
 	Set s = empty_set();
-	
+
 	int opt = 0;
 	if(argc == 1){
 		printf("Select at least one sort to perform.\n");
@@ -72,18 +86,19 @@ int main(int argc, char **argv) {
 				//s = insert_set(INSERTION, s);
 				break;
 			case 'e':
-				s = insert_set(HEAP, s);
+			//	s = insert_set(s, HEAP);
 				break;
 			case 'i':
 				s = insert_set(INSERTION, s);
 				break;
 			case 's':
-				s = insert_set(SHELL, s);
+			//	s = insert_set(s, SHELL);
 				break;
 			case 'q':
-				s = insert_set(QUICK, s);
+			//	s = insert_set(s, QUICK);
 				break;
 			case 'n':
+				
 				// this is the length of array
 				break;
 			case 'p':
@@ -92,24 +107,14 @@ int main(int argc, char **argv) {
 			case 'r':
 				// set seed
 				break;
+			default:
+				printf("./sorting: option requires an argument");
 			}
 	}
 
-	srandom(SEED);
-	uint32_t *A = (uint32_t *)calloc(LENGTH, sizeof(uint32_t));
-	for(uint32_t i = 0; i <= LENGTH; i += 1){
-		A[i] = random();
-		// implement a bitmask to fit in 30 bits
-	}
-
-	Stats stats;
-	stats.moves = 0;
-	stats.compares = 0; 
-
 	for (Sorts x = INSERTION; x <= QUICK; x += 1){
-		if(member_set(x, s)){
-			if(x == 0){
-				insertion_sort(&stats, A, 100);
+		if(member_set(x , s)){
+				sort[x](&stats, A, LENGTH);
 				printf("%s", names[x]);
 				printf(" %d elements ",  LENGTH);
         			printf("%" PRIu64 " moves", stats.moves);
@@ -123,19 +128,7 @@ int main(int argc, char **argv) {
 				printf("\n");
 				reset(&stats);
 			}
-			if(x == 1){
-				printf("Heap sort\n");
-			}
-			if(x == 2){
-				printf("Shell sort\n");
-			}
-			if(x == 3){
-				printf("Quick sort\n");
-			}
 		}
-			
-		//same with heap, quick, shell
-	}
 	free(A);
 	return 0;
 }
