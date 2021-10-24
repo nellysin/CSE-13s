89c883af7b63ab8b57cfba29f5b1a61fb2cf08b5
Nelly Sin <nesin@ucsc.edu>
2021-10-24 20:53:55 +0000

    i started the beginning of this week and I'm not finished yet

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -100,34 +100,47 @@ int main(int argc, char **argv) {
     //This part has not yet been finished but these were my ideas:
     //opening the file (after doing the command -i) -- structure CITE: TUTOR James
     // read in the number of vertices 'n'
-    char buffer[1024]; //CITE: TA Eugene
+
     uint32_t i = 0, j = 0, k = 0;
-    uint32_t n = 0;
-
-    fscanf(infile, "%" SCNu32 "\n", &n);
-    printf("%" PRIu32 "\n", n);
-
-    //read in the line by how many vertices
-    for (uint32_t t = 0; t < n; t += 1) {
-        fgets(buffer, 1024, infile);
-        printf("%s", buffer);
-        //maybe create an array special to cities
+    uint32_t vert = 0;
+    //getting the vertices
+    fscanf(infile, "%" SCNu32 "\n", &vert);
+    printf("%" PRIu32 "\n", vert);
+    if(vert > VERTICES){
+	    fprintf(stderr, "Vertices out of bounds.");
     }
-    //create graph 'G'
 
-    //add edge to graph 'G' and reading edges
-    while (fscanf(infile, "%" SCNu32 " %" SCNu32 " %" SCNu32 "\n", &i, &j, &k) != EOF) {
+    //create graph
+    Graph *G = graph_create(vert, undirected);
+
+    char **cities = (char **) calloc(vert, sizeof(char *));
+    //read in the line by how many vertices
+    char buffer[1024];
+    while (fgets(buffer, 1024, infile) != NULL){ //reading the file lines
+        for (uint32_t m = 0; m < vert; m += 1) { //indicate the city names in file
+            fgets(buffer, 1024, infile); 
+            strdup(buffer);
+            cities[m] = buffer; //create array for cities
+            printf("%s", cities[m]);
+        }
         fscanf(infile, "%" SCNu32 " %" SCNu32 " %" SCNu32 "\n", &i, &j, &k);
         printf("%" PRIu32 " %" PRIu32 " %" PRIu32 "\n", i, j, k);
-        //call graph edge to assign i, j, and k in the graph
+        //graph_add_edge(G, i, j, k); //add edge to graph 'G' and reading edges
     }
+
     //create current path
+    Path *curr = path_create();
 
     //create shortest path
+    Path *shortest = path_create();
     //call dfs
-
+    
     //print results
 
     //free paths and graphs before returning!!
+    path_delete(&shortest);
+    path_delete(&curr);
+    graph_delete(&G);
+    fclose(infile);
     return 0;
 }
