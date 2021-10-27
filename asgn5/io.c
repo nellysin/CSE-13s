8f2930f5260398db7c4d62b37986d97792444d79
Nelly Sin <nesin@ucsc.edu>
2021-10-27 05:00:48 +0000

    yay

--- asgn5/io.c
+++ asgn5/io.c
@@ -12,24 +12,29 @@ uint64_t bytes_read;
 uint64_t bytes_written;
 
 int read_bytes(int infile, uint8_t *buf, int nbytes){
-	while(buf[nbytes] > 0){
-		bytes_read += read(infile, buf, nbytes);
-		nbytes -= 1;
+	nbytes = read(infile, buf, nbytes);
+	bytes_read = 0;
+	while((uint64_t) nbytes != bytes_read){
+		read(infile, buf, nbytes);
+		bytes_read += 1;
 	}
 	return bytes_read;
 }
 
 int write_bytes(int outfile, uint8_t *buf, int nbytes){
-	while(buf[nbytes] > 0){
+	bytes_written = 0;
+	while((uint64_t) nbytes != bytes_read){
 		write(outfile, buf, nbytes);
 	}
-	return outfile;
+	return bytes_written;
 }
 
 int main(void){
 	uint8_t buf[256];
 	int infile = open("test.txt", O_RDONLY);
+
 	read_bytes(infile, buf, 256);
-	printf("Bytes read: %"PRIu64" ", bytes_read);
+	printf("Bytes read: %"PRIu64"\n", bytes_read);
+	printf("Bytes written: %"PRIu64"\n", bytes_written);
 	return 0;
 }
