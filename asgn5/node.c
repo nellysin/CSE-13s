1b7d005e0ca2d8e0cb2280ce6ea242f101fd0440
Nelly Sin <nesin@ucsc.edu>
2021-10-29 07:23:29 +0000

    finishing and understanding node

--- asgn5/node.c
+++ asgn5/node.c
@@ -3,21 +3,29 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+//CITE: Professor Long for sudo code
+//CITE: TA Eugene for structure
+
 Node *node_create(uint8_t symbol, uint64_t frequency){
-	Node *n = (Node *)calloc(1, sizeof(Node));
-	n->symbol = symbol;
+	Node *n = (Node *)calloc(1, sizeof(Node)); //initalizing the calloc
+	n->symbol = symbol; //initiliazing the symbol and frequency in node
 	n->frequency = frequency;
 	return n;
 }
 
 void node_delete(Node **n){
-	free(n);
-	n = NULL;
+	free(*n);	// delete the node after you're done
+	*n = NULL;
 	return;
 }
 
 Node *node_join(Node *left, Node *right){
-	parent->frequency = left->frequency + right->frequency;
-	Node *parent = node_create('$', parent->frequency);
-	return parent;
+	uint64_t parent_Node = (left->frequency + right->frequency); //node is the addition of the left and right frequency
+	Node *parent = node_create('$', parent_Node); //creating the parent node from node create
+	parent->left = left; //initializing left and right
+	parent->right = right; 
+	return parent; //returning parent
+}
+
+void node_print(Node *n){
 }
