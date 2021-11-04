41928654ae3377019be5d106cff2fff6b9b5b5d4
Nelly Sin <nesin@ucsc.edu>
2021-11-04 00:42:26 +0000

    debugging enqueue and dequeue

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -59,68 +59,63 @@ bool enqueue(PriorityQueue *q, Node *n) { //CITE: TA Eugene for enqueue sudo cod
     if (pq_full(q) == true) { //if it is full
         return false;
     }
-    if (q->tail == 0) {		//if it is empty
+    if (pq_empty(q) == true) { //if it is empty
         q->items[q->tail] = n;
         q->tail += 1;
         return true;
     } else {
-        int parent = q->tail; //the parent 
-        q->items[parent] = n;
+        int parent = q->tail; //initialize parent to q->tail (make it a little cleaner)
+        q->items[parent] = n; //assign items[parent] to the node
         q->tail += 1;
-        while (parent > 1) { //while k > 1 <- this will be the index
+        while (q->tail > 1 
+               && q->items[parent]->frequency
+                      < q->items[parent / 2]->frequency) { //while k > 1 <- this will be the index and comparing the frequency
+            Node *temp = node_create(0, 0); //if the parent frequency is smaller then swap by creating a temporary node
+            temp = q->items[parent / 2]; // place the [parent / 2] into the temp
+            q->items[parent / 2] = q->items[parent]; //the [parent] will be assigned to [parent / 2]
+            q->items[parent] = temp; // then assign temp to [parent]
             parent = (parent / 2); //parent = floor(k/2)
-            if (n->frequency < q->items[parent]->frequency) {
-                Node *temp = node_create(0, 0);
-                temp = q->items[parent];
-                q->items[parent] = n;
-                n = temp;
-            }
         }
         return true;
     }
 }
-void min_child(Node **f, first, last)
 
-void fix_heap(Node **f, int first, int last){
-	bool found = false;
-	uint32_t left = parent * 2;
-	uint32_t right = left + 1;
-	uint32_t min = first;
-	
-	if(left <q->capacity && f[left]->frequency < f[parent]->frequency){ //check if the left freq is less than parent
-		min = f[left]->frequency;
-	}else{
-		min = f[parent]->frequency; //the min is still the parent
-	}
-	if(f[right]->frequency < f[parent]->frequency){ //check if the right freq is less than parent
-		min = f[right]->frequency; //make min to the right freq.
-	} else{
-		min = f[parent]->frequency; //make min to the parent freq
-	}
-	if(min != parent){	//when the min isn't parent
-		Node *temp = node_create(0,0); //this is swapping using a new Node *temp
-		temp = f[parent];
-		f[parent] = f[min];
-		f[min] = temp;
-		found = true;
-		}
+void heapify(
+    PriorityQueue *q, uint32_t parent) { //CITE: TA Eugene & Tutor Ben for heapify and dequeue
+    uint32_t left = (parent * 2);
+    uint32_t right = (left + 1);
+    uint32_t min = parent;
+
+    if ((left < q->capacity)
+        && (q->items[min]->frequency
+            < q->items[left]
+                  ->frequency)) { //check if the left freq is less than parent and a stop function for capacity
+        min = left;
+    }
+    if ((right < q->capacity)
+        && (q->items[min]->frequency
+            < q->items[right]->frequency)) { //check if the right freq is less than parent
+        min = right; //make min to the right freq.
+    }
+    if (min != parent) { //when the min isn't parent
+        Node *temp = node_create(0, 0); //this is swapping using a new Node *temp
+        temp = q->items[parent];
+        q->items[parent] = q->items[min];
+        q->items[min] = temp;
+        heapify(q, min);
+    }
+    return;
 }
 
-*bool dequeue(PriorityQueue *q, Node **n) {  //CITE Ben for help on fix heap and dequeue
-	if(pq_empty(q) == true){ //return false when pq is empty
-		return false;
-	}
-	if(q->tail == 1){
-		q->tail += 1;
-		*n = q->items[0];
-		return true;
-	}
-	*n = q->items[0];
-	uint32_t s = (q->tail - 1);
-	q->[0] = q->items[s];
-	q->tail -= 1;
-	fix_heap(q, 1);
-	return true;
+bool dequeue(PriorityQueue *q, Node **n) { //CITE Ben for help on fix heap and dequeue
+    if (pq_empty(q) == true) { //return false when pq is empty
+        return false;
+    }
+    *n = q->items[0];
+    q->items[0] = q->items[q->tail - 1];
+    q->tail -= 1;
+    heapify(q, 0);
+    return true;
 }
 
 void pq_print(PriorityQueue *q) {
