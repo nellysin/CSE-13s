f953bd923fffb570dcd5192cca5b77c1bed8ddbd
Nelly Sin <nesin@ucsc.edu>
2021-11-05 00:31:06 +0000

    complete Makefile and debug io.c

--- asgn5/io.c
+++ asgn5/io.c
@@ -1,4 +1,4 @@
-/*#include "io.h"
+#include "io.h"
 #include "defines.h"
 #include "code.h"
 
@@ -13,15 +13,14 @@ uint64_t bytes_read;
 uint64_t bytes_written;
 static uint8_t buf[BLOCK];
 static int index = 0; //CITE: TA Eugene for the statics (tack bit in the buffer)
-static int end = -1; // tack the last valid bit
-static uint8_t writeflush[BLOCK];
+static uint8_t wfbuf[BLOCK];
 
 //CITE: Professor Long
 //CITE: TA Eugene for help on read byte
 //CITE: TA Sloan for help on read byte
 //CITE: Miles for help on read bit
 
-int read_bytes(int infile, uint8_t *buf, int nbytes) { //CITE: TA Eugene for sudo
+/*int read_bytes(int infile, uint8_t *buf, int nbytes) { //CITE: TA Eugene for sudo
     uint64_t i = nbytes; //changing expression
     while (i != bytes_read) { //reading the specified bytes by nbytes
         uint64_t bytes = read(infile, (buf + bytes_read),
@@ -47,25 +46,35 @@ int write_bytes(int outfile, uint8_t *buf, int nbytes) { //CITE: TA Eugene for s
     return bytes_written; //return bytes_written once it stops the while loop
 }*/
 
-/*int read_bytes(int infile, *buf, int nbytes) { //CITE: TA Eugene for read_bytes sudo code
-    int bytes = read(infile, buf + bytes_read, nbytes - read_bytes); //bytes will call read
-    int bytes_read = 0; //bytes_read <- this is what we will be returning
-    while (bytes > 0) { // if bytes is greater than 0
-        bytes;
-        bytes_read += bytes
-    }
-    return bytes_read;
+int read_bytes(int infile, uint8_t *buf, int nbytes) { //CITE: TA Eugene for read_bytes sudo code & Tutor Eric
+	int bytes = -1; 
+	int total_bytes = 0;
+	while ((total_bytes != nbytes) && (bytes != 0)){
+		bytes = read(infile, buf, nbytes); //read function that read the infile
+		total_bytes += bytes; //adding total bytes read
+		buf += bytes; //add buffer
+		nbytes -= bytes; //decrement nbytes
+	}
+	bytes_read += total_bytes; //read bytes will transfer to the bytes_read and return bytes_read
+	return bytes_read; //returning the bytes_read
 }
 
-int write_bytes(int outfile, *buf, int nbytes) { //CITE: TA Eugene for write_bytes sudo code
-    int bytes = write(outfile, *buf + bytes_write, nbytes - bytes_write);
-    int bytes_read = 0;
-    while (bytes > 0) {
-        bytes;
-        bytes_write += bytes;
-    }
+int write_bytes(int outfile, uint8_t *buf, int nbytes) { //CITE: TA Eugene for write_bytes sudo code & Tutor Eric
+	int bytes = -1;
+	int total_bytes = 0;
+	while((total_bytes != nbytes) && (bytes != 0)){
+		bytes = write(outfile, buf, nbytes); //read function that wirte to the outfile
+		total_bytes += bytes;  // add the bytes to total_bytes
+		buf += bytes;  // this will add the bytes to the buffer
+		nbytes -= bytes; //decrement the nbytes
+	}
+	bytes_written += total_bytes; //this is for assigning total_bytes to bytes_written
+	return bytes_written; //returning bytes_written
 }
-bool read_bit(int infile, uint8_t *bit) { // CITE: TA Eugene
+
+bool read_bit(int infile, uint8_t *bit) { // CITE: TA Eugene & Tutor Eric
+    static int end = -1;
+    int bit_block = (8 * BLOCK);
     if (index == 0) { //if the buf is empty, fill it
         int bytes = read_bytes(infile, buf, BLOCK);
         if (bytes < BLOCK) {
@@ -75,47 +84,43 @@ bool read_bit(int infile, uint8_t *bit) { // CITE: TA Eugene
         }
     }
     //return a bit out of a buffer
-    *bit = code_get_bit(bit, index);
-    index += 1;
+    *bit = (buf[index/8] >> (index % 8)) & 0x1;
+    index = ((index + 1) / (bit_block));
 
-    if (index == BLOCK * 8) { //when to reset the index
-        index = 0;
-    }
     //returning true/false
-    return index != end;
+    return (index == (8 * bit_block));
 }
 
-void write_code(int outfile, Code *c) { //CITE: TA Eugene
+void write_code(int outfile, Code *c) { //CITE: TA Eugene & Tutor Eric
     //buffers a code
     //buffer is written when filled
 
     //buffer each bit into the buffer
-    for (uint32_t i = 0; i < code_size(c); i += 1) {
-        int b = code_get_bit(c, i);
-//        if (b == 1) {
-            //set the bit at index
-//            c->bits[i / MAX_CODE_SIZE]
-//                |= (0x1 << i % MAX_CODE_SIZE); //CITE: Professor Long for the setbit from bv8
-
-        } else {
-            //clearing the bit
-            writeflush[i] &= ~(0x1 << i %); //CITE: Professor Long from comments repo
-        }
-        index += 1;
-        //if buffer is filled (if index == BLOCK *8)
-        if (index == BLOCK * 8) {
-            flush_codes(outfile);
-        }
-        index = 0;
+    for (uint32_t i = 0; i < code_size(c); i += 1) { //if the bit in c-> bits at index 1
+	uint8_t bit = code_get_bit(c, i);    
+    	if(bit == 1){
+		//set the bit
+		wfbuf[index / 8] |= (0x1 << (index % 8));
+	}
+	else{
+		//clear the bit
+		wfbuf[index / 8] &= ~(0x1 << (index % 8));
+    	}
+	index += 1;
+	if(index == BLOCK * 8){
+		flush_codes(outfile);
+	}
     }
     return;
 }
-
-void flush_codes(int outfile) {
+void flush_codes(int outfile) { //CITE: TA Eugene
     //flush bits out of buffer if not empty
-    if (index > 0) {
-
-        //convert index into bytes
-        //write_bytes(outfile, index);
+    uint8_t byte = 0;
+    if((index % 8) == 0){
+	   byte = (index / 8);
+    } else {
+	    byte = ((index / 8) + 1);
     }
-}*/
+    write_bytes(outfile, wfbuf, byte);
+    return;
+}
