d7180fbe5227fb6357a9ad7e3d0db8af909479f7
Nelly Sin <nesin@ucsc.edu>
2021-11-09 08:25:17 +0000

    adding makefile and others

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -1,10 +1,22 @@
 #include "numtheory.h"
 
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <gmp.h>
 
-void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus){
+void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
+    mpz_t v, p;
+    mpz_init_set_ui(v, 1);
+    mpz_init_set(p, base);
+    while (mpz_cmp_ui(exponent, 0)) {
+        if (1 == gmp_mod(exponent, 2)) {
+            x = mpz_mul(x, p);
+            x = gmp_mod(modulus);
+        }
+        p = mpz_mul(p, p);
+        p = gmp_mod(modulus);
+        exponent = gmp_div_q(exponent, 2);
+    }
+    return v;
 }
--- asgn6/randstate.c
+++ asgn6/randstate.c
@@ -3,12 +3,12 @@
 #include <stdint.h>
 #include <gmp.h>
 
-void randstate_init(uint64_t seed){
-	gmp_randstate_t state;
-	gmp_randinit_mt(state);
-	gmp_randinit_ui(state, seed);
+void randstate_init(uint64_t seed) {
+    gmp_randstate_t state;
+    gmp_randinit_mt(state);
+    gmp_randinit_ui(state, seed);
 }
 
-void randstate_clear(void){
-	gmp_randclear(state);
+void randstate_clear(void) {
+    gmp_randclear(state);
 }
