cfcd2a1c19469216c6c24df18389e3bb93783ce0
Nelly Sin <nesin@ucsc.edu>
2021-11-11 06:23:26 +0000

    now to debug

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -1,4 +1,5 @@
 #include "numtheory.h"
+#include "randstate.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -29,50 +30,78 @@ void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
 
 bool is_prime(mpz_t n, uint64_t iters){
 	mpz_t n_minus, r, two;
-	mp_bitcount_t s = 2;
+	mp_bitcnt_t s = 2; //this is just a bit counter -- in 2^s
         mpz_inits(n_minus,r, two, NULL);
 	mpz_sub_ui(n_minus, n, 1);
-	mpz_set_ui(two, 2);
-	mp_bitcount_ts = 2; //this is just a bit counter -- in 2^s 
+	mpz_set_ui(two, 2); 
 	
-	while(mps_divisible_2xp_p(n_minus, s)){//break until it is not divisible by 2
+	while(mpz_divisible_2exp_p(n_minus, s)){//break until it is not divisible by 2
 		s += 1;		
 	}
 	s -= 1;
-	mpz_tdiv_q_2exp(r, n_minus, s)//storing it to r
+	mpz_tdiv_q_2exp(r, n_minus, s);//storing it to r
 	
 	mpz_t a, bound, y, j, s_minus;//we need a bound because we want a to be between 2 to n - 1
 	mpz_inits(a, bound, y,j, s_minus, NULL);
-	for(i = 0; i < iters, i += 1){
+	for(uint64_t i = 0; i < iters; i += 1){
 		mpz_sub_ui(bound, n, 3);
-		mpz_urandom(a,state, bound); //this returns 0 to n - 1 (this is not inclusive, therefore it's n - 2)
+		mpz_urandomm(a,state, bound); //this returns 0 to n - 1 (this is not inclusive, therefore it's n - 2)
 		mpz_add_ui(a, a, 2);
 		pow_mod(y, a, r, n);
-		mpz_sub_ui(s_minus, s, 1)
-		if((mpz_cmp_ui(y, 1) != 0)) && (mpz_cmp(n_minus,y) != 0){
+		if((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(n_minus,y) != 0)){
 			
 			mpz_set_ui(j, 1);
 			
-			while((mpz_cmp_ui(j, s_minus) <= 0) && (mpz_cmp(y, n_minus) != 0)){
+			while((mpz_cmp_ui(j, s) <= 0) && (mpz_cmp(y, n_minus) != 0)){
 				pow_mod(y, y, two, n);
 				if(mpz_cmp_ui(y, 0) == 0){
 					return false;
 				}
 				mpz_add_ui(j, j, 1);
 			}
-			if(mpz_cmp_ui(y, n_minus) != 0){
+			if(mpz_cmp(y, n_minus) != 0){
 				return false;
 			}
 
 		}
 	}
+	mpz_clears(n_minus, r, two, a, bound, y, j, s_minus, NULL); //no memory leak
 	return true;
 }
 
 void make_prime(mpz_t p, uint64_t bits, uint64_t iters){
 	do{
-		mpz_urandom(p,state, bits);
+		mpz_urandomb(p,state, bits);
 	} while(!is_prime(p, iters));
 }
 
+void mod_inverse(mpz_t o, mpz_t a, mpz_t n){
+	mpz_t r, r1, t, t1, q, tempr, tempt;
+	mpz_inits(r, r1, t, t1, q, tempr, tempt, NULL);
+	mpz_set(r, n);
+	mpz_set(r1, a);
+	mpz_set_ui(t, 0);
+	mpz_set_ui(t1, 1);
+	while(mpz_cmp_ui(r1, 0) != 0){
+		//this is r and r1
+		
+		mpz_fdiv_q(q, r, r1);
+		mpz_set(tempr, r);
+		mpz_set(r, r1);
+		mpz_mul(r1, q, r1);
+		mpz_sub(r1, tempr, r1);
 
+		//this is t and t1
+		mpz_set(tempt, t);
+		mpz_set(t, t1);
+		mpz_mul(t1, q, t1);
+		mpz_sub(t1, tempt, t1);
+	}
+	if(mpz_cmp_ui(r, 1) > 0){
+		mpz_set(o, 0);
+	}
+	if(mpz_cmp_ui(t, 0) < 0){
+		mpz_add(t, t, n);
+	}
+	mpz_clears(r, r1, t, t1, q, tempr, tempt, NULL);
+}
