df5cc6494d8f504d0d6d2498a41ea0f0a6c3a6ae
Nelly Sin <nesin@ucsc.edu>
2021-11-11 07:42:04 +0000

    no idea how to debug

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -105,7 +105,7 @@ void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
     if (mpz_cmp_ui(r, 1) == 1) {
         mpz_set(o, 0);
     }
-    if (mpz_cmp_ui(t, 0) == -1) {
+    else if (mpz_cmp_ui(t, 0) == -1) {
         mpz_add(o, t, n);
     }
     mpz_clears(r, r1, t, t1, q, tempr, tempt, NULL);
