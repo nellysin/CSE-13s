a34718b7537e2e2c30f124bf7d78dca428dce7ea
Nelly Sin <nesin@ucsc.edu>
2021-11-11 06:29:23 +0000

    adding additional to compile

---
+++ asgn6/decrypt.c
@@ -0,0 +1,9 @@
+#include "numtheory.h"
+#include "randstate.h"
+
+#include <stdio.h>
+#include <gmp.h>
+
+int main(void) {
+    return 0;
+}
---
+++ asgn6/encrypt.c
@@ -0,0 +1,10 @@
+
+#include "numtheory.h"
+#include "randstate.h"
+
+#include <stdio.h>
+#include <gmp.h>
+
+int main(void) {
+    return 0;
+}
