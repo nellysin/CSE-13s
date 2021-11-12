ef0195acfb2e20c55c835c84092fc2038af8327b
Nelly Sin <nesin@ucsc.edu>
2021-11-12 09:23:24 +0000

    debugging is_prime

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -1,4 +1,3 @@
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -6,7 +5,8 @@
 
 #include "numtheory.h"
 #include "randstate.h"
-gmp_randstate_t state;
+
+//gmp_randstate_t state;
 
 //CITE: Professor Long (pseudo for number theory)
 //CITE: TA Eugene (structure) (11/9 section)
@@ -33,46 +33,62 @@ void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
 
 bool is_prime(mpz_t n, uint64_t iters) {
     mpz_t n_minus, r, two; //initialize the mpz
+
     //mpz_init(n); //initializing
+    //this is for writing n - 1 = 2^s(r) such that r is odd
     mpz_inits(n_minus, r, two, NULL);
 
-    mp_bitcnt_t s = 2; //this is just a bit counter -- in 2^s
     mpz_sub_ui(n_minus, n, 1); //this will be n - 1
+
+    mp_bitcnt_t s = 2; //this is just a bit counter -- in 2^s
     mpz_set_ui(two, 2); //setting an mpz_t dedicated to 2
     //such that r is odd
+    
+    if(mpz_cmp_ui(n, 2) < 0 || (mpz_cmp_ui(n, 4) == 0)){ //M Rambin only works for n > 2 and 4 fails 
+        return false;
+    }
+    if(mpz_cmp_ui(n, 4) < 0){ // 3 is prime but it would still fail (considering range is {2 ... n - 2})
+        return true;
+    }
+
     while (mpz_divisible_2exp_p(n_minus, s)) { // n-1 / 2^s = r
         s += 1; //then increment the s
     }
-    mpz_tdiv_q_2exp(r, n_minus, s); //storing it to r = (n-1)/ 2^s
     s -= 1; // b/c 0 < r < s - 1
 
-    mpz_t a, bound, y, j; //we need a bound because we want a to be between 2 to n - 1
-    mpz_inits(a, bound, y, j, NULL);
+    mpz_tdiv_q_2exp(r, n_minus, s); //storing it to r = (n-1)/ 2^s
 
-    for (uint64_t i = 1; i < iters; i += 1) { //iterating through the number of iters
-        mpz_sub_ui(bound, n, 3); //setting the upper bound to be n - 3
+    mpz_t a, bound, y, j; //we need a bound because we want a to be between 2 to n - 1
+    mpz_inits(a, bound, y, j, NULL); // another set of initialization
+
+    for (uint64_t i = 0; i < iters; i += 1) { //iterating through the number of iters
+	mpz_sub_ui(bound, n, 3); //setting the upper bound to be n - 3
         mpz_urandomm(a, state,
             bound); //this returns 0 to n - 1 (this is not inclusive, therefore it's n - 2)
-        mpz_add_ui(a, a, 2); //
+        mpz_add_ui(a, a, 2); 
         pow_mod(y, a, r, n);
         if ((mpz_cmp_ui(y, 1) != 0)
-            && (mpz_cmp(n_minus, y) != 0)) { // comparing if y != 1 and y != n-1
+            && (mpz_cmp(y, n_minus) != 0)) { // comparing if y != 1 and y != n-1
 
             mpz_set_ui(j, 1);
 
             while ((mpz_cmp_ui(j, s) <= 0) && (mpz_cmp(y, n_minus) != 0)) {
                 pow_mod(y, y, two, n);
                 if (mpz_cmp_ui(y, 1) == 0) {
-                    return false;
+                    // printf("not prime"); //testing
+		     return false;
                 }
                 mpz_add_ui(j, j, 1);
             }
             if (mpz_cmp(y, n_minus) != 0) {
+		//printf("not prime"); //testing
                 return false;
             }
+
         }
     }
     mpz_clears(n_minus, r, two, a, bound, y, j, NULL); //no memory leak
+    //printf("is prime"); //testing
     return true;
 }
 
@@ -83,62 +99,58 @@ void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
 }
 
 void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
-    mpz_t r, r1, t, t1, q, tempr, tempt;
+    mpz_t r, r1, t, t1, q, tempr, tempt; //initialize
     mpz_inits(r, r1, t, t1, q, tempr, tempt, NULL);
 
     //mpz_init(o);
     //mpz_init(a);
     //mpz_init(n);
 
-    mpz_set(r, n);
-    mpz_set(r1, a);
-    mpz_set_ui(t, 0);
+    mpz_set(r, n); //(r,r′) ←(n,a)
+    mpz_set(r1, a); 
+    mpz_set_ui(t, 0); //(t,t′) ←(0,1)
     mpz_set_ui(t1, 1);
-    while (mpz_cmp_ui(r1, 0) != 0) {
+    while (mpz_cmp_ui(r1, 0) != 0) { //while r′ != 0
         //this is r and r1
 
-        mpz_fdiv_q(q, r, r1);
-        mpz_set(tempr, r);
-        mpz_set(r, r1);
-        //mpz_submul(tempr, q, r1);
-        //mpz_set(r1, tempr);
+        mpz_fdiv_q(q, r, r1);  //q ← r/r′
+        mpz_set(tempr, r);    //(r,r′) ←(r′,r−q×r′)
+        mpz_set(r, r1);		//(using a temp variable to not overwrite r)
         mpz_mul(r1, q, r1);
         mpz_sub(r1, tempr, r1);
 
         //this is t and t1
-        mpz_set(tempt, t);
-        mpz_set(t, t1);
-        //mpz_submul(tempt, q, t1);
-        //mpz_set(t1, tempt);
+        mpz_set(tempt, t); //(t,t′) ←(t′,t−q×t′)
+        mpz_set(t, t1);     //using a temp variable to not overwrite t 
         mpz_mul(t1, q, t1);
         mpz_sub(t1, tempt, t1);
     }
-    if (mpz_cmp_ui(r, 1) > 0) {
+    if (mpz_cmp_ui(r, 1) > 0) { //if r > 1
         mpz_set_ui(o, 0);
-        return;
+        return;  //return no inverse
     }
     if (mpz_cmp_ui(t, 0) < 0) {
         mpz_add(t, t, n);
         //mpz_set(o,t);
     }
-    mpz_set(o, t);
+    mpz_set(o, t); //storing in outfile
     mpz_clears(r, r1, t, t1, q, tempr, tempt, NULL);
-    return;
+    return; //return o
 }
 
 void gcd(mpz_t g, mpz_t a, mpz_t b) {
     mpz_t t;
-    mpz_init(t);
+    mpz_init(t); //initialize (clear to me)
 
     //mpz_init(a);
     //mpz_init(g);
     //mpz_init(b);
 
-    while (mpz_cmp_ui(b, 0) != 0) {
-        mpz_set(t, b);
-        mpz_mod(b, a, b);
-        mpz_set(a, t);
+    while (mpz_cmp_ui(b, 0) != 0) { //while b != 0
+        mpz_set(t, b); // set b to t
+        mpz_mod(b, a, b); //assign a % b to b
+        mpz_set(a, t); // assign t to a
     }
-    mpz_set(g, a);
-    mpz_clears(t);
+    mpz_set(g, a); //setting a to g 
+    mpz_clear(t); //clearing
 }
