edfe4d5506c0363b1d0265c5a4722bdcd322a866
Nelly Sin <nesin@ucsc.edu>
2021-11-13 03:09:43 +0000

    debugging

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -14,21 +14,25 @@
 //CITE: Tutor Eric (is_prime & make prime) (11/10 session)
 
 void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
-    mpz_t v, p, odd; //initialize v and p
-    mpz_init(odd);
+    mpz_t v, p, odd, temp; //initialize v and p
+    mpz_inits(odd, temp, NULL);
+    
+    mpz_set(temp, d);
+
     mpz_init_set_ui(v, 1); //initialize the set of v with 1
     mpz_init_set(p, a); //initialize the set with the base
-    while (mpz_cmp_ui(d, 0) > 0) { //compare the exponent to be greater than 0
-        if (1 == mpz_mod_ui(odd, d, 2)) { //if the exponent is odd
+    
+    while (mpz_cmp_ui(temp, 0) > 0) { //compare the exponent to be greater than 0
+        if (1 == mpz_mod_ui(odd, temp, 2)) { //if the exponent is odd
             mpz_mul(v, v, p); //do operation v = v x p
             mpz_mod(v, v, n); //and find the modulus of that
         }
         mpz_mul(p, p, p); //multiply the p with itself
         mpz_mod(p, p, n); //and assgn it to the modulus of n
-        mpz_fdiv_q_ui(d, d, 2); //divide d by 2
+        mpz_fdiv_q_ui(temp, temp, 2); //divide d by 2
     }
     mpz_set(o, v); //store the computed result in out.
-    mpz_clears(v, p, odd, NULL); //no memory leak
+    mpz_clears(v, p, odd, temp, NULL); //no memory leak
 }
 
 bool is_prime(mpz_t n, uint64_t iters) {
@@ -43,8 +47,10 @@ bool is_prime(mpz_t n, uint64_t iters) {
     mp_bitcnt_t s = 0; //this is just a bit counter -- in 2^s
     mpz_set_ui(two, 2); //setting an mpz_t dedicated to 2
 
-    mpz_tdiv_q_2exp(r, n_minus, s); //storing it to r = (n-1)/ 2^s
+    //mpz_tdiv_q_2exp(r, n_minus, s); //storing it to r = (n-1)/ 2^s
     
+    mpz_sub_ui(r, n, 1);
+
     //base cases
     if(mpz_cmp_ui(n, 2) < 0 || (mpz_cmp_ui(n, 4) == 0)){ //M Rambin only works for n > 2 and 4 fails 
         
@@ -63,41 +69,39 @@ bool is_prime(mpz_t n, uint64_t iters) {
 	mpz_fdiv_q_ui(r, r, 2);
     }
 
-//    gmp_printf("r: %Zd \n", r);
-//    printf("s: %lu \n", s);
-//    printf("iters: %lu \n", iters);
+    gmp_printf("r: %Zd \n", r);
+    printf("s: %lu \n", s);
 
     mpz_t a, bound, y, j; //we need a bound because we want a to be between 2 to n - 1
     mpz_inits(a, bound, y, j, NULL); // another set of initialization
 
-    for (uint64_t i = 0; i < iters; i += 1) { //iterating through the number of iters
+    for (uint64_t i = 1; i < iters; i += 1) { //iterating through the number of iters
 	mpz_sub_ui(bound, n, 3); //setting the upper bound to be n - 3
-        mpz_urandomm(a, state,
+	mpz_urandomm(a, state,
             bound); //this returns 0 to n - 1 (this is not inclusive, therefore it's n - 2)
-        mpz_add_ui(a, a, 2); 
-        pow_mod(y, a, r, n);
-        if ((mpz_cmp_ui(y, 1) != 0)
+
+	mpz_add_ui(a, a, 2);  
+	pow_mod(y, a, r, n);
+        
+	if ((mpz_cmp_ui(y, 1) != 0)
             && (mpz_cmp(y, n_minus) != 0)) { // comparing if y != 1 and y != n-1
 
             mpz_set_ui(j, 1);
 
-            while ((mpz_cmp_ui(j, (s-1)) <= 0) && (mpz_cmp(y, (n_minus)) != 0)) {
+            while ((mpz_cmp_ui(j, s - 1) <= 0) && (mpz_cmp(y, (n_minus)) != 0)) {
                 pow_mod(y, y, two, n);
                 if (mpz_cmp_ui(y, 1) == 0) {
-                    // printf("not prime"); //testing
 		     return false;
                 }
                 mpz_add_ui(j, j, 1);
             }
             if (mpz_cmp(y, n_minus) != 0) {
-		//printf("not prime"); //testing
                 return false;
             }
 
         }
     }
     mpz_clears(n_minus, r, two, a, bound, y, j, NULL); //no memory leak
-    //printf("is prime"); //testing
     return true;
 }
 
