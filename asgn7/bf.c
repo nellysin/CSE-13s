0bcf5ee28b8a0ad7dd0c22cdb04045ebeb0b6c1d
Nelly Sin <nesin@ucsc.edu>
2021-11-23 08:22:59 +0000

    i am adding bf.c because my internet was down today :(

---
+++ asgn7/bf.c
@@ -0,0 +1,104 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "bv.h"
+#include "salts.h"
+#include "speck.h"
+#include "bf.h"
+
+//CITE: Professor Long for the struct
+//CITE: TA Eugene for asgn doc
+//CITE: Tutor Jason for explanation for the hashing and bloom filter
+
+struct BloomFilter{ //struct for bloom filter
+	uint64_t primary[2];
+	uint64_t secondary[2];
+	uint64_t tertiary[2];
+	BitVector *filter;
+};
+
+BloomFilter *bf_create(uint32_t size){
+	BloomFilter *bf; 
+	bf = (BloomFilter *) calloc(1, sizeof(BloomFilter)); //calloc the bf
+
+	bf->filter = (BitVector *) calloc(size, sizeof(uint8_t));
+
+	bf->primary[0] = SALT_PRIMARY_LO;  // setting primary, secondary, tertiary
+	bf->primary[1] = SALT_PRIMARY_HI;  // setting primary, secondary, tertiary
+	
+	bf->secondary[0] = SALT_SECONDARY_LO;
+	bf->secondary[1] = SALT_SECONDARY_HI;
+
+	bf->tertiary[0] = SALT_TERTIARY_LO;
+	bf->tertiary[1] = SALT_TERTIARY_HI;
+
+	return bf;
+}
+void bf_delete(BloomFilter **bf){
+	free((*bf)->filter);
+	free(*bf); //free bf and filter for no memory leaks
+	*bf = NULL;
+}
+
+uint32_t bf_size(BloomFilter *bf){
+	return bv_length(bf->filter); //return the length of the bloomfilter w/ the bitvector length function
+}
+
+void bf_insert(BloomFilter *bf, char *oldspeak){
+	uint32_t hashprim = hash(bf->primary, oldspeak);
+	hashprim = hashprim % bf_size(bf);
+	bv_set_bit(bf->filter, hashprim);
+	
+	uint32_t hashsec = hash(bf->secondary, oldspeak);
+	hashsec = hashsec % bf_size(bf);
+	bv_set_bit(bf->filter, hashsec);
+	
+	uint32_t hashter = hash(bf->tertiary, oldspeak);
+	hashter = hashter % bf_size(bf);
+	bv_set_bit(bf->filter, hashter);
+
+}
+
+
+bool bf_probe(BloomFilter *bf, char *oldspeak){
+	uint32_t hashprim = hash(bf->primary, oldspeak);
+	hashprim = hashprim % bf_size(bf);
+	if(bv_set_bit(bf->filter, hashprim)){
+		return true;
+	} else {
+		return false;
+	}
+
+        uint32_t hashsec = hash(bf->secondary, oldspeak);
+	hashsec = hashsec % bf_size(bf);
+	if(bv_set_bit(bf->filter, hashsec)){
+		return true;
+	} else{
+		return false;
+	}
+
+        uint32_t hashter = hash(bf->tertiary, oldspeak);	
+	hashter = hashter % bf_size(bf);
+	if(bv_set_bit(bf->filter, hashter)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+uint32_t bf_count(BloomFilter *bf){
+	uint32_t count = 0;
+	for(uint32_t i = 0; i < bf_size(bf); i += 1){
+		if(bv_get_bit(bf->filter, i) == 1){
+			count += 1;
+		}
+	}
+	return count;
+}
+
+void bf_print(BloomFilter *bf);/*{
+	for(uint32_t i = 0; i < bf_size(bf); i += 1){
+		if()
+	}
+}*/
