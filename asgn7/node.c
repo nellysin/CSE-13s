5c637516327875d890aed369e4813132303eb817
Nelly Sin <nesin@ucsc.edu>
2021-11-23 08:37:04 +0000

    checking node.c

--- asgn7/node.c
+++ asgn7/node.c
@@ -32,7 +32,9 @@ void node_delete(Node **n){
 	*n = NULL;
 }
 
-/*void node_print(Node *n){
+void node_print(Node *n);/*{
+
+	if(n->oldspeak != NULL)
 	printf("%s -> %s\n", n->oldspeak, n->newspeak);
 	printf("%s\n", n->oldspeak);
 }*/
