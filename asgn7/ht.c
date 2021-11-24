29856869ba0ccfa60eab35f7713e8bf431ef5d17
Nelly Sin <nesin@ucsc.edu>
2021-11-24 09:52:11 +0000

    adding counts

--- asgn7/ht.c
+++ asgn7/ht.c
@@ -7,6 +7,8 @@
 #include "salts.h"
 #include "speck.h"
 
+//CITE: Professor Long in Lecture slides 28
+
 struct HashTable {
     uint64_t salt[2];
     uint32_t size;
@@ -41,20 +43,20 @@ uint32_t ht_size(HashTable *ht) {
 }
 
 Node *ht_lookup(HashTable *ht, char *oldspeak) {
-    if (bst_find(*ht->trees, oldspeak) != NULL) {
-        return *ht->trees;
-    } else {
-        return false;
-    }
+	uint32_t index = hash(ht->salt, oldspeak) % ht_size(ht);
+
+	return bst_find(ht->trees[index], oldspeak);
 }
 
 void ht_insert(HashTable *ht, char *oldspeak, char *newspeak) {
-    bst_insert(*ht->trees, oldspeak, newspeak);
+    uint32_t index = hash(ht->salt, oldspeak) % ht_size(ht);
+
+    ht->trees[index] = bst_insert(ht->trees[index], oldspeak, newspeak);
 }
 uint32_t ht_count(HashTable *ht) {
     uint32_t count = 0;
     for (uint32_t i = 0; i < ht->size; i += 1) {
-        if (ht->trees[i] != NULL) {
+        if (ht->trees[i]) {
             count += 1;
         }
     }
@@ -62,5 +64,20 @@ uint32_t ht_count(HashTable *ht) {
 }
 
 double ht_avg_bst_size(HashTable *ht) {
-    return bst_size(*ht->trees) / ht_count(ht);
+    uint32_t total = 0;
+    for(uint32_t i = 0; i < ht_size(ht); i += 1){
+	total += bst_size(ht->trees[i]);
+    }
+    double average;
+    return average = total / ht_count(ht);
+
 }
+
+double ht_avg_bst_height(HashTable *ht){
+	double total;
+	uint32_t to_divide = 0;
+	for(uint32_t i = 0; i < ht_size(ht); i += 1){
+		to_divide += bst_height(ht->trees[i]);
+	}
+	return total = to_divide / ht_count(ht);
+}	
